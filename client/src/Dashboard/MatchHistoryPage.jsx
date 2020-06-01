4e51a2ad2d229332fe60701fe401627267ae0e4c
Tian <t2fu@ucsd.edu>
2020-05-31 19:58:51 -0700

    Working match history page

--- client/src/Dashboard/MatchHistoryPage.jsx
+++ client/src/Dashboard/MatchHistoryPage.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {Component, useContext, useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import MatchCard from "./MatchCard";
+import MatchCard2 from "./MatchCard2";
+import {Grid} from "@material-ui/core";
+import {AuthContext} from "../auth/Auth";
 
 const useStyles = makeStyles((theme) => ({
   h2: {
@@ -13,15 +15,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MatchHistoryPage() {
-  const classes = useStyles();
+export default function MatchHistoryPage(props){
+  const { currentUser } = useContext(AuthContext);
+  const match = props.match;
+  //const names = match.name;
+  const [state, setState] = useState({
+    curTime : new Date().toLocaleString()
+  });
+
+  function componentDidMount() {
+    setInterval( () => {
+      this.setState({
+        curTime : new Date().toLocaleString()
+      })
+    },1000)
+  }
+  const names = [];
+  for (let i = 0; i < match.length; i++){
+    if (match[i].time > state.curTime ) {
+      //names.push(<p>{match[i].name}</p>)
+      // eslint-disable-next-line no-unused-expressions
+      for (let j = 0; j < match[i].roster.length; j++) {
+        if (match[i].roster[j].email === currentUser.email) {
+        names.push(<MatchCard2 match={match[i]}/>);
+      }
+     }
+    }else{
+        return(<h2>You don't have any match yet!</h2>)
+    }
+  }
   return (
-    <div>
-      {/* <h2 className={classes.h2}>Active Matches</h2>
+      <div>
+        {/* <h2 className={classes.h2}>Active Matches</h2>
             <MatchCard />
             <h2 className={classes.h2}>Past Matches</h2>
             <MatchCard />
-            <MatchCard /> */}
-    </div>
+            <MatchCard /> */
+
+        }
+
+        <p>Current Time : {state.curTime} Current User: {currentUser.email}</p>
+        {names}
+      </div>
   );
 }
